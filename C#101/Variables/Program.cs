7480d360189129f3582a8a35419bc0d8ecc9566e
Dilay Canitez <92249675+dcanitez@users.noreply.github.com>
2022-01-09 23:16:30 +0200

    Variables HW added

---
+++ C#101/Variables/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte a = 5;     // 1 byte  deger araligi: 0 - 255
+            sbyte b = 5;    // 1 byte  deger araligi: -128 - 127
+
+            short c = 5;    // 2 byte  deger araligi: -32768 - 32768
+            ushort us = 5;  // 2 byte  deger araligi: 0 - 65365
+            
+            int i = 2;      // 4 byte
+            Int16 i16 = 2;  // 2 byte
+            Int32 i32 = 2;  // 4 byte
+            Int64 i64 = 2;  // 8 byte
+
+            uint ui = 2;    // 4 byte
+            long l = 4;     // 8 byte
+            ulong ul = 4;   // 8 byte
+            
+            //Reel sayılar
+            float f = 5;    // 4 byte
+            double d = 5;   // 8 byte
+            decimal de = 5; // 16 byte
+
+            char ch = '2';      // 2 byte
+            string str = "Dilay";  //sınırsız
+
+            bool b1 = true;
+            bool b2 = false;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 = "x";
+            object o2 = 'y';
+            object o3 = 4;
+            object o4 = 4.3;
+
+            // string ifadeler
+
+            string str1 = string.Empty;
+            str = "Dilay Canitez";
+            string name = "Dilay";
+            string surname = "Canitez";
+            string fullName = name + " " + surname;
+
+            //integer tanımlama şekilleri
+
+            int integer1 = 5;
+            int integer2 = 3;
+            int integer3 = integer1 * integer2;
+
+            //boolean
+
+            bool bool1 = 10>2;
+
+            // Değişken Dönüşümleri
+
+            string str20 = "20";
+            int integer20 = 21;
+            string yeniDeger = str20 + integer20.ToString();
+            Console.WriteLine(yeniDeger); 
+
+            int integer21 = integer20 + Convert.ToInt32(str20);
+            Console.WriteLine(integer21); 
+
+            int integer22 = integer20 + int.Parse(str20);
+            Console.WriteLine(integer22); 
+
+            // datetime
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(datetime);
+
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(datetime2);
+
+            //saat
+            string hour = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(hour);
+
+                        
+        }
+    }
+}
